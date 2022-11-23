b141a66698e3f114e2a4f24e4c26234d4cd81a45
Surabhi951 <surabhibhagat95@gmail.com>
2022-11-23 21:10:01 +0530

    Modified: Created linked list by appending 30 and 70 to 56

--- src/com/bridgelabz/LinkedListMain.java
+++ src/com/bridgelabz/LinkedListMain.java
@@ -1,9 +1,21 @@
 package com.bridgelabz;
 
+import java.util.Scanner;
+
 public class LinkedListMain {
 
     public static void main(String[] args) {
-        Operations.addDataAtStart();
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the options:\n1. Add the data.\n2. Append the data");
+        switch (sc.nextInt()) {
+
+            case 1:
+                Operations.addDataAtStart();
+                break;
+            case 2:
+                Operations.append();
+                break;
+        }
 
     }
 }
