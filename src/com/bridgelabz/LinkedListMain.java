33a4ce2111de0427d9d1d0e2b650e60e90110c39
Surabhi951 <surabhibhagat95@gmail.com>
2022-11-24 17:54:50 +0530

    Modified: Inserted 30 between 56 and 70

--- src/com/bridgelabz/LinkedListMain.java
+++ src/com/bridgelabz/LinkedListMain.java
@@ -6,7 +6,7 @@ public class LinkedListMain {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the options:\n1. Add the data.\n2. Append the data");
+        System.out.println("Enter the options:\n1. Add the data.\n2. Append the data\n3. Insert in between");
         switch (sc.nextInt()) {
 
             case 1:
@@ -14,6 +14,9 @@ public class LinkedListMain {
                 break;
             case 2:
                 Operations.append();
+                break;
+            case 3:
+                Operations.insertInBetween();
                 break;
         }
 
